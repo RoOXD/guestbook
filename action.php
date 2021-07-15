8f4d3ecbffbdf9c01f930d34cf0826daf2a0d7ac
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-15 15:54:52 +0300

    second commit

--- action.php
+++ action.php
@@ -2,15 +2,14 @@
 
 $dbconn = pg_connect("host=localhost port=5432 dbname=guestbook user=postgres");
 
-$name = $_POST['name'];
-$note = $_POST['note'];
+$name = htmlspecialchars($_POST['name'],ENT_QUOTES);
+$note = htmlspecialchars($_POST['note'],ENT_QUOTES);
 
-if(ctype_alnum($name)||ctype_alnum($note)){
-	$query = pg_query($dbconn, "INSERT INTO guestbook (author, notes) VALUES ('$name','$note')");
-	if ( $query ) {
-	    echo  "Adaugat!";
-	}else{
-		echo "Esec!";
-	}
-}else {echo "Caractere nepermise!";}
+$query = pg_query_params($dbconn, 'INSERT INTO guestbook (author, notes) VALUES ($1,$2)', array($name,$note));
+if ( $query ) {
+          echo  "Adaugat!";
+      }else{
+              echo "Esec!";
+      }
+
 ?>
